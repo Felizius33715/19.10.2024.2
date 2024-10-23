08a2f525ec6a484160bbb76c9906e13334e3f261
Felizius <Felizius@yandex.ru>
2024-10-23 21:48:15 +0700

    UPD

--- 19.10.2024.2.cpp
+++ 19.10.2024.2.cpp
@@ -52,13 +52,6 @@ public:
 	}
 
 	smartArray(const smartArray &other) {
-			/*capacity = other.capacity;
-			size_arr = other.size_arr;
-			Arr = new int[capacity];
-			for (int i = 0; i < size_arr; ++i) {
-				Arr[i] = other.Arr[i];
-			}
-		return *this;*/
 		capacity = other.capacity;
 		size_arr = other.size_arr;
 		Arr = new int[capacity];
